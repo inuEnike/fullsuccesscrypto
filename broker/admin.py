fa79ab6e83765b8ca09805a8cff41ea5014489c2
inuEnike <inuenike@gmail.com>
2024-08-10 05:21:07 -0700

    first commit

---
+++ broker/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register (Customer)
+admin.site.register (Deposit)
+admin.site.register (Profile)
+admin.site.register (Account)
+admin.site.register(Wiretransfer)
+admin.site.register(Banktransfer)
+admin.site.register(Bitcoin)
+admin.site.register(Kyc)
+admin.site.register (Wallet)
+admin.site.register (Pin)
